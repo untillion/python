4821cfc591377e3292e8cf3be187e6eabf142ebc
untillion <wcure001@fiu.edu>
2017-08-20 10:43:24 -0400

    Create replication.py

---
+++ replication.py
@@ -0,0 +1,24 @@
+# Input:  A string Text and an integer k
+# Output: CountDict(Text, k)
+def CountDict(Text, k):
+    Count = {} 
+    for i in range(len(Text)-k+1):
+        Pattern = Text[i:i+k]
+        Count[i] = PatternCount(Pattern, Text)
+    return Count
+    # fill in the rest of the CountDict function here.
+
+# Copy your PatternCount function from Replication.py into the line below.
+def PatternCount(Pattern, Text):
+    count = 0 # output variable
+    # your code here
+    count = 0
+    for i in range(len(Text)-len(Pattern)+1):
+        if Text[i:i+len(Pattern)] == Pattern:
+            count = count+1
+    return count
+
+### DO NOT MODIFY THE CODE BELOW THIS LINE ###
+import sys
+lines = sys.stdin.read().splitlines()
+print(CountDict(lines[1],int(lines[0])))
