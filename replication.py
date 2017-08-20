3fbf8c2b591cc68fbdc9cbbd08e8332c0e3fbf9f
untillion <wcure001@fiu.edu>
2017-08-20 10:55:27 -0400

    Update replication.py
    added Frequentwords function

--- replication.py
+++ replication.py
@@ -1,14 +1,28 @@
+def FrequentWords(Text, k):
+    FrequentPatterns = [] # output variable
+    # your code here
+    Count = CountDict(Text, k)
+    m = max(Count.values())
+    for i in Count:
+        if Count[i] == m:
+            FrequentPatterns.append(Text[i:i+k])
+    return FrequentPatterns
+
 # Input:  A string Text and an integer k
 # Output: CountDict(Text, k)
+# HINT:   This code should be identical to when you last implemented CountDict
 def CountDict(Text, k):
+    Count = {} # output variable
+    # your code here
     Count = {} 
     for i in range(len(Text)-k+1):
         Pattern = Text[i:i+k]
         Count[i] = PatternCount(Pattern, Text)
     return Count
-    # fill in the rest of the CountDict function here.
 
-# Copy your PatternCount function from Replication.py into the line below.
+# Input:  Strings Pattern and Text
+# Output: The number of times Pattern appears in Text
+# HINT:   This code should be identical to when you last implemented PatternCount
 def PatternCount(Pattern, Text):
     count = 0 # output variable
     # your code here
@@ -17,8 +31,7 @@ def PatternCount(Pattern, Text):
         if Text[i:i+len(Pattern)] == Pattern:
             count = count+1
     return count
-
 ### DO NOT MODIFY THE CODE BELOW THIS LINE ###
 import sys
 lines = sys.stdin.read().splitlines()
-print(CountDict(lines[1],int(lines[0])))
+print(' '.join(FrequentWords(lines[0],int(lines[1]))))
