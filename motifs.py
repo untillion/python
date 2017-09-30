aaf06760fd982e4d076f61412021b113c8f1189f
untillion <wcure001@fiu.edu>
2017-09-30 17:02:28 -0400

    Update motifs.py
    Add consensus function

--- motifs.py
+++ motifs.py
@@ -1,3 +1,18 @@
+def Consensus(Motifs):
+    # insert your code here
+    k = len(Motifs[0])
+    count = Count(Motifs)
+    consensus = ""
+    for j in range(k):
+        m = 0
+        frequentSymbol = ""
+        for symbol in "ACGT":
+            if count[symbol][j] > m:
+                m = count[symbol][j]
+                frequentSymbol = symbol
+        consensus += frequentSymbol
+    return consensus
+
 def Profile(Motifs):
     t = len(Motifs)
     k = len(Motifs[0])
