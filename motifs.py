1a42fb7c8cacef3e077cde09c18feb9ed8e51584
untillion <wcure001@fiu.edu>
2017-09-30 16:54:27 -0400

    Update motifs.py
    Added profile matrix calculator to Motifs.py

--- motifs.py
+++ motifs.py
@@ -1,3 +1,18 @@
+def Profile(Motifs):
+    t = len(Motifs)
+    k = len(Motifs[0])
+    profile = {}
+    # insert your code here
+    CountMotifs = Count(Motifs)
+    for symbol in "ACGT":
+        profile[symbol] = []
+        for x in CountMotifs:
+            for y in (CountMotifs[symbol]):
+                z = y/float(t)
+                profile[symbol].append(z)
+        
+    return profile
+
 def Count(Motifs):
     count = {} # initializing the count dictionary
     # your code here
