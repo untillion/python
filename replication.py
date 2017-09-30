95a3e71e266c69fea2cd45d9c156559624d1d182
untillion <wcure001@fiu.edu>
2017-09-30 13:52:20 -0400

    Update replication.py
    remove functions from motif.py

--- replication.py
+++ replication.py
@@ -81,18 +81,5 @@ def ApproximatePatternMatching(Pattern, Text, d):
         if PatternDist < d+1:
                          positions.append(i)
     return positions
-def Count(Motifs):
-    count = {} # initializing the count dictionary
-    # your code here
-    k = len(Motifs[0])
-    for symbol in "ACGT":
-        count[symbol] = []
-        for j in range(k):
-             count[symbol].append(0)
-    t = len(Motifs)
-    for i in range(t):
-        for j in range(k):
-            symbol = Motifs[i][j]
-            count[symbol][j] += 1
-    return count
+
 
